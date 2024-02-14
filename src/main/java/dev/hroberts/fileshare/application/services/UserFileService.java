49a4d7db12cb151f233363420d901b1fe2476b4c
Harrison Roberts <harrisonp.roberts@gmail.com>
2024-02-13 23:24:57 -0700

    Added auth filter

--- src/main/java/dev/hroberts/fileshare/application/services/UserFileService.java
+++ src/main/java/dev/hroberts/fileshare/application/services/UserFileService.java
@@ -5,6 +5,8 @@ import dev.hroberts.fileshare.application.domain.SharedFileInfo;
 import dev.hroberts.fileshare.application.mappers.SharedFileInfoMapper;
 import dev.hroberts.fileshare.persistence.filestore.IFileStore;
 import dev.hroberts.fileshare.persistence.repositories.FileInfoRepository;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.PathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,14 +16,18 @@ import java.util.UUID;
 
 @Service
 public class UserFileService {
-
+    final String host;
     final FileInfoRepository fileInfoRepository;
     final IFileStore localFileStore;
 
-    public UserFileService(FileInfoRepository fileInfoRepository, IFileStore localFileStore) {
+    public UserFileService(FileInfoRepository fileInfoRepository,
+                           IFileStore localFileStore, @Value("${host}") String host) {
         this.fileInfoRepository = fileInfoRepository;
         this.localFileStore = localFileStore;
+        this.host = host;
     }
+
+    public PathResource downloadFile(String )
 
     public PathResource downloadFileById(UUID fileId) throws IOException {
         var fileInfo = fileInfoRepository.getFileById(fileId);
@@ -42,7 +48,13 @@ public class UserFileService {
         fileInfo.fileName = file.getOriginalFilename();
         fileInfo.downloadLimit = downloadLimit;
         fileInfo.fileId = fileId;
+        fileInfo.downloadUrl = generateDownloadUrl();
         fileInfoRepository.saveFileInfo(fileInfo);
         return SharedFileInfoMapper.MapDomainToDto(fileInfo);
+    }
+
+    private String generateDownloadUrl() {
+        var shortId = RandomStringUtils.randomAlphabetic(8);
+        return host + "/download/" + shortId;
     }
 }
---
+++ src/main/java/dev/hroberts/fileshare/filters/AuthenticationFilter.java
@@ -0,0 +1,26 @@
+package dev.hroberts.fileshare.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class AuthenticationFilter extends OncePerRequestFilter {
+    @Value("${api-key}") String apiKey;
+    final String API_HEADER = "X-API-KEY";
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestApiKey = request.getHeader(API_HEADER);
+        if (apiKey.equals(requestApiKey) || apiKey.equals("dev")) {
+            filterChain.doFilter(request, response);
+        } else {
+            filterChain.doFilter(request, response);
+
+         //   response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid API key");
+        }
+    }
+}
