6d86cd756626537178918addb45ea88f24f34333
Harrison Roberts <harrisonp.roberts@gmail.com>
2024-05-19 17:39:45 +1200

    Basic broken chunking

--- src/main/resources/static/scripts/upload.js
+++ src/main/resources/static/scripts/upload.js
@@ -1,11 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const chunkSize = (1024 * 1024 * 10);
     let dropArea = document.getElementById('drop_zone');
     dropArea.addEventListener('dragover', dragOverHandler, false)
     dropArea.addEventListener('drop', dropHandler, false)
 
     let dirty = false;
     const conditionalDivs = Array.from(document.getElementsByClassName('conditional-display'));
-    const filesToUpload = [];
+    const filesToUpload = Array.from([]);
     const selectedFiles = document.getElementById('selected-files');
 
     function dragOverHandler(ev) {
@@ -22,9 +23,12 @@ window.addEventListener('DOMContentLoaded', () => {
             [...ev.dataTransfer.items].forEach((item, i) => {
                 // If dropped items aren't files, reject them
                 if (item.kind === "file") {
+                    console.log('file.');
                     const file = item.getAsFile();
                     addFileToUpload(file);
                     console.log(`… file[${i}].name = ${file.name}`);
+                } else {
+                    console.log('not file?', item);
                 }
             });
         } else {
@@ -35,12 +39,20 @@ window.addEventListener('DOMContentLoaded', () => {
                 console.log(`… file[${i}].name = ${file.name}`);
             });
         }
+        read();
+        //doUpload();
     }
 
     function addFileToUpload(file) {
         console.log('adding file to upload')
         console.log('checking file', file);
         console.log('against list of files to upload', filesToUpload);
+
+        //todo allow multi file uploads at some point
+        if(filesToUpload.length > 0) {
+            return;
+        }
+
         if(filesToUpload.some(item => item.name === file.name)) {
             console.log('duplicate file found');
             return;
@@ -104,6 +116,41 @@ window.addEventListener('DOMContentLoaded', () => {
         console.log(file);
         console.log(filesToUpload);
         refreshView();
+    }
+
+    function uploadChunk(range) {
+
+        let kb = file.slice(0, 10);
+        let reader = new FileReader();
+        reader.onload = (e) => {
+            let chunk = e.target.result;
+            console.log(chunk.toString());
+
+            let enc = new TextDecoder('utf-8');
+            console.log(enc.decode(chunk));
+        }
+        reader.readAsArrayBuffer(kb);
+    }
+
+    function doUpload() {
+        //todo calculate num chunks
+        let file = filesToUpload[0];
+        let size = file.size;
+        let numChunks = Math.ceil(size / chunkSize);
+
+        console.log('number of chunks in file', numChunks);
+
+        //todo initiate
+        //todo send chunks off
+        let pos = 0;
+        while(pos < size) {
+            let chunk;
+            if((size - pos) >= chunkSize) {
+                chunk = uploadChunk(file.slice(pos, (pos + chunkSize)));
+            } else {
+                chunk = uploadChunk(file.slice(pos, size));
+            }
+        }
     }
 });
 
